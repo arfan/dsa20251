70163176f38ea18e122fb35ec7b42bb1df78dbd2
abdul.arfan <abdul.arfan@gmail.com>
2025-10-01 18:48:21 +0700

    sample insert using recursive

--- week5/sample_binary_tree.py
+++ week5/sample_binary_tree.py
@@ -1,28 +1,41 @@
+from collections import deque
+
 class Node:
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, data):
+        self.data = data
         self.left = None
         self.right = None
+
+
+def insert_node(root, data):
+    if root is None:
+        return Node(data)
+    
+    if data < root.data:
+        root.left = insert_node(root.left, data)
+    elif data > root.data:
+        root.right = insert_node(root.right, data)
+
+    return root
 
 def print_tree(node, level=0, prefix=""):
     if node is None:
         return
     
     # Print current node
-    print("  " * level + prefix + str(node.value))
+    print("  " * level + prefix + str(node.data))
     
     if node.left is not None:
-        print_tree(node.left, level + 1, "└── ")
+        print_tree(node.left, level + 1, "left: ")
     if node.right is not None:
-        print_tree(node.right, level + 1, "└── ")
+        print_tree(node.right, level + 1, "right: ")
     
         
 a = Node(4)
-b = Node(3)
-c = Node(6)
-
 root = a
-a.left = b
-a.right = c
+
+root = insert_node(root, 3)
+root = insert_node(root, 5)
+root = insert_node(root, 1)
 
 print_tree(root)
